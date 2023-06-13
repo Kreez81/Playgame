63b7d21e4cf8fee1f7f2446fba7f0ff9e3d01906
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2023-06-13 21:22:09 +0530

    help form working

--- src/HelpPage/Help.jsx
+++ src/HelpPage/Help.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Navbar from '../Widgets/Navbar'
+import emailjs from "@emailjs/browser";
+import Botbar from '../Widgets/Botbar';
 
 function Help() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+  //   var Params = {
+  //     full_name: document.getElementById("full_name").value,
+  //     email_id: document.getElementById("email_id").value
+  // };
+
+    emailjs.sendForm('service_9iratng', 'template_c0s2qop', form.current, 'slqbPA4Z8ny6DVV6H')
+      .then((result) => {
+          console.log(result.text);
+          alert("Message sent")
+      }, (error) => {
+          console.log(error.text);
+      });
+      form.current.reset();
+  };
+
   return (
     <>
     <Navbar />
-    <div style={{backgroundColor:"#19758c", fontSize:"5vh", display:"flex", justifyContent:"center",alignItems:"center",height:"100vh"}}>IN PROGRESS</div>
+    <div style={{backgroundColor:"#19758c", fontSize:"3vh", display:"flex", justifyContent:"center",alignItems:"center",height:"100vh"}}>
+    <form style={{display:"flex", flexDirection:"column"}} ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" id='full_name' name="full_name" />
+      <label>Email</label>
+      <input type="email" id='email_id' name="email_id" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+    </div>
+    <Botbar />
     </>
   )
 }
